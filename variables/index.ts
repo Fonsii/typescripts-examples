263a9efb5886c8bc09b57e6b8761b7a5b917c1ed
Fonsii <lualfonso14200174@gmail.com>
2022-09-29 14:50:36 -0600

    Example variables

---
+++ variables/index.ts
@@ -0,0 +1,29 @@
+var text : String = "Hello World";
+//text = 1
+//console.log(text);
+
+//sayGreeting();
+
+function sayGreeting(){
+    let greeting : String = "Hello Adventurers";
+    console.log(greeting);
+}
+
+//console.log(greeting);
+
+//sayHello(1);
+//sayHello("Luis");
+
+function sayHello(name : String){
+    console.log("Hello " + name);
+}
+
+var number = 1
+//number = "EE"
+
+const running = true;
+checkRunning();
+function checkRunning(){
+    console.log(running);
+}
+
